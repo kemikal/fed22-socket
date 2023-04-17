374d0c8937b1ba8e5e7fa7248e7ba34176c86777
kemikal <janne@kemikal.org>
2023-04-17 11:10:13 +0200

    first commit

---
+++ klient/script.js
@@ -0,0 +1,17 @@
+import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+const socket = io("http://localhost:3000");
+
+let sendMessage = document.getElementById("sendMessage");
+let sendBtn = document.getElementById("sendBtn");
+let messages = document.getElementById("messages");
+
+socket.on("chat", (arg) => {
+    console.log("chat", arg);
+
+    messages.innerHTML += arg.chat + " | from: " + arg.user + "<br/>";
+})
+
+sendBtn.addEventListener("click", () => {
+    socket.emit("chat", {chat: sendMessage.value, user: "Janne"});
+})
+
